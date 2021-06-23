0e4d86df115f6c65987e449b9003238001317862
mahendrabp <chipxitro@gmail.com>
2021-06-23 14:30:14 +0700

    feat: add update note function

---
+++ src/handlers/updateNote.ts
@@ -0,0 +1,42 @@
+import { DynamoDB } from 'aws-sdk';
+import createError from 'http-errors';
+
+const dynamoDB = new DynamoDB.DocumentClient();
+
+async function updateNote(event: any) {
+  if (!event.pathParameters?.id) {
+    throw new createError.BadRequest('parameter ID must be provided');
+  }
+
+  if (!event.body) {
+    throw new createError.BadRequest('Event body must be provided');
+  }
+
+  const data: any = JSON.parse(event.body);
+
+  const params: DynamoDB.DocumentClient.Update = {
+    TableName: process.env.NOTES_API_TABLE_NAME as string,
+    Key: {
+      noteId: event.pathParameters.id,
+    },
+    UpdateExpression: 'SET content = :content',
+    ExpressionAttributeValues: {
+      ':content': data.content || '',
+    },
+    ReturnValuesOnConditionCheckFailure: 'ALL_NEW',
+  };
+
+  try {
+    await dynamoDB.update(params).promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'success update' }),
+  };
+}
+
+export const handler = updateNote;
